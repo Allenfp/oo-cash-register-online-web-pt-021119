3d1140801a1f034c8fabb009ff0cc2f3d1c28220
Allen F Partlow <allenfp@gmail.com>
2019-03-17 01:40:46 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,7 +1,7 @@
 
 class CashRegister
 
-  attr_accessor :total :discount
+  attr_accessor :total
 
   def initialize(discount=0.0)
     @total = 0.0
@@ -10,10 +10,6 @@ class CashRegister
 
   def discount
     @discount.to_i
-  end
-
-  def total
-    @total
   end
 
 end
