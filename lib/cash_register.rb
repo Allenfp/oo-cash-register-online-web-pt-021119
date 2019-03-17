ec44ee91f88a0bb3798ef215cc2a0aa0d8b6f55c
Allen F Partlow <allenfp@gmail.com>
2019-03-17 01:42:20 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -6,11 +6,15 @@ class CashRegister
   def initialize(discount=0.0)
     @total = 0.0
     @discount = discount
+    @items = []
   end
 
   def discount
     @discount.to_i
   end
 
+  def add_item(item, price)
+
+  end
 
 end
